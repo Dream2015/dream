91c7be66498555f59d8975e856674230a38d686c
Dongnan <1005021307@qq.com>
2016-05-29 22:31:11 +0800

    Update

--- app/src/main/java/com/android/dongnan/androidapi/MainActivity.java
+++ app/src/main/java/com/android/dongnan/androidapi/MainActivity.java
@@ -1,13 +1,113 @@
 package com.android.dongnan.androidapi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ListView listView = new ListView(this);
+
+        PackageAdapter adapter = new PackageAdapter(getPackageData(getIntent()));
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        setContentView(listView);
+
+    }
+
+    private ArrayList<String> getPackageData(Intent intent)  {
+        ArrayList<String> data = new ArrayList<>();
+
+        return  data;
+    }
+
+    private class PackageAdapter implements ListAdapter {
+        private ArrayList<String> mData = new ArrayList<>();
+
+        public PackageAdapter(ArrayList<String> data) {
+            mData = data;
+        }
+
+        @Override
+        public void registerDataSetObserver(DataSetObserver observer) {
+
+        }
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver observer) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView view = new TextView(parent.getContext());
+            view.setText(mData.get(position));
+            view.setTag(mData.get(position));
+            return view;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return 0;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return true;
+        }
     }
 }
