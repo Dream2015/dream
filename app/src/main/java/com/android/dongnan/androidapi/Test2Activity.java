522ec6156eafea7b9a28cc6018feb84d72e21774
Dongnan <1005021307@qq.com>
2016-05-29 21:52:24 +0800

    Add Test Activity

--- app/src/main/java/com/android/dongnan/androidapi/Test2Activity.java
+++ app/src/main/java/com/android/dongnan/androidapi/Test2Activity.java
@@ -7,14 +7,14 @@ import android.widget.TextView;
 /**
  * Created by dream on 16/5/29.
  */
-public class TestActivity extends Activity{
+public class Test2Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         TextView view = new TextView(this);
-        view.setText("Test 1");
+        view.setText("Test2");
 
         setContentView(view);
     }
