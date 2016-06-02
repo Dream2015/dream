6c6d17c17669d763430f6912aca9361d2d6eb924
Dongnan <1005021307@qq.com>
2016-06-02 22:13:44 +0800

    Check Style

--- app/src/main/java/com/android/dongnan/androidapi/view/TextSwitcherTest.java
+++ app/src/main/java/com/android/dongnan/androidapi/view/TextSwitcherTest.java
@@ -23,7 +23,7 @@ public class TextSwitcherTest extends Activity {
     private int mCount = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         LinearLayout layout = new LinearLayout(this);
@@ -31,21 +31,24 @@ public class TextSwitcherTest extends Activity {
         layout.setGravity(Gravity.CENTER);
 
         Button button = new Button(this);
-        button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+        button.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT));
         button.setText("Next");
         layout.addView(button);
 
         mSwitcher = new TextSwitcher(this);
         mSwitcher.setFactory(mViewFactory);
-        mSwitcher.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
-        mSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
+        mSwitcher.setInAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_in));
+        mSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_out));
         mSwitcher.setText(String.valueOf(mCount));
         layout.addView(mSwitcher);
 
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
                 mCount++;
                 mSwitcher.setText(String.valueOf(mCount));
             }
@@ -54,13 +57,18 @@ public class TextSwitcherTest extends Activity {
         setContentView(layout);
     }
 
-    ViewSwitcher.ViewFactory mViewFactory = new ViewSwitcher.ViewFactory() {
+    /**
+     * Add view to TextSwitcher.
+     */
+    private ViewSwitcher.ViewFactory mViewFactory =
+            new ViewSwitcher.ViewFactory() {
         @Override
         public View makeView() {
             TextView view = new TextView(TextSwitcherTest.this);
             view.setGravity(Gravity.CENTER);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                view.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
+                view.setTextAppearance(
+                        android.R.style.TextAppearance_DeviceDefault_Large);
             }
             return view;
         }
