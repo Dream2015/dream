87222818bc6068acb77a236523ee542c17649173
Dongnan <1005021307@qq.com>
2016-06-02 21:47:07 +0800

    Add animation to TextSwitcher

--- app/src/main/java/com/android/dongnan/androidapi/view/TextSwitcherTest.java
+++ app/src/main/java/com/android/dongnan/androidapi/view/TextSwitcherTest.java
@@ -1,12 +1,13 @@
 package com.android.dongnan.androidapi.view;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Switch;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
@@ -14,6 +15,7 @@ import android.widget.ViewSwitcher;
 
 /**
  * Created by dream on 16/6/2.
+ * TextSwitcher - add FactoryView
  */
 public class TextSwitcherTest extends Activity {
 
@@ -36,6 +38,8 @@ public class TextSwitcherTest extends Activity {
 
         mSwitcher = new TextSwitcher(this);
         mSwitcher.setFactory(mViewFactory);
+        mSwitcher.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+        mSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
         mSwitcher.setText(String.valueOf(mCount));
         layout.addView(mSwitcher);
 
@@ -55,6 +59,9 @@ public class TextSwitcherTest extends Activity {
         public View makeView() {
             TextView view = new TextView(TextSwitcherTest.this);
             view.setGravity(Gravity.CENTER);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                view.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
+            }
             return view;
         }
     };
