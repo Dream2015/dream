79859e21c1879a4b9efdb2857fcbff4d27999e99
Dongnan <1005021307@qq.com>
2016-06-03 22:27:12 +0800

    Good Luck

--- app/src/main/java/com/android/dongnan/androidapi/view/FloatingButtonViewTest.java
+++ app/src/main/java/com/android/dongnan/androidapi/view/FloatingButtonViewTest.java
@@ -2,9 +2,18 @@ package com.android.dongnan.androidapi.view;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Outline;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewOutlineProvider;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.dongnan.androidapi.R;
 
 /**
  * Created by dream on 16/6/3.
@@ -15,27 +24,12 @@ public class FloatingButtonViewTest extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FloatingButton button = new FloatingButton(this);
-        setContentView(button);
+        TextView view = new TextView(this);
+        view.setText("Good Luck");
 
-    }
 
-    private class FloatingButton extends FrameLayout {
 
-        public FloatingButton(Context context) {
-            this(context, null);
-        }
+        setContentView(R.layout.floating_action_button);
 
-        public FloatingButton(Context context, AttributeSet attrs) {
-            this(context, attrs, 0);
-        }
-
-        public FloatingButton(Context context, AttributeSet attrs, int defStyleAttr) {
-            this(context, attrs, defStyleAttr, 0);
-        }
-
-        public FloatingButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-            super(context, attrs, defStyleAttr);
-        }
     }
 }
