b3f61c028da54a10aaed6103637f43d13707e255
Dongnan <1005021307@qq.com>
2016-06-04 23:20:19 +0800

    Rating done

--- app/src/main/java/com/android/dongnan/androidapi/view/RatingTest.java
+++ app/src/main/java/com/android/dongnan/androidapi/view/RatingTest.java
@@ -2,43 +2,31 @@ package com.android.dongnan.androidapi.view;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
-import com.android.dongnan.androidapi.R;
-
 /**
  * Created by dream on 16/6/4.
  */
-public class RatingTest extends Activity implements View.OnClickListener, RatingBar.OnRatingBarChangeListener{
+public class RatingTest extends Activity implements RatingBar.OnRatingBarChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         LinearLayout layout = new LinearLayout(this);
+        layout.setGravity(Gravity.CENTER);
+        layout.setOrientation(LinearLayout.VERTICAL);
 
         RatingBar ratingBar = new RatingBar(this);
         ratingBar.setNumStars(5);
         ratingBar.setRating(2);
         ratingBar.setOnRatingBarChangeListener(this);
-        ratingBar.setOnClickListener(this);
 
         layout.addView(ratingBar);
         setContentView(layout);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v instanceof RatingBar) {
-            RatingBar bar = (RatingBar)v;
-            Toast.makeText(this,
-                    "Value is:" + bar.getRating() + bar.getNumStars(),
-                    Toast.LENGTH_LONG)
-                    .show();
-        }
     }
 
     @Override
