305666183130ecb48a8b22db49db0227761be010
Dongnan <1005021307@qq.com>
2016-06-16 22:51:15 +0800

    Health OS

---
+++ app/src/main/java/com/android/dongnan/androidapi/view/HealthTools.java
@@ -0,0 +1,16 @@
+package com.android.dongnan.androidapi.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dream on 16/6/16.
+ */
+public class HealthTools extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
