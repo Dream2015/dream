84a4c2bd8caafbbfaf4e2e6ae8f7138d23a7dc09
Dongnan <1005021307@qq.com>
2017-02-09 22:50:22 +0800

    Add Debug Model

--- common/src/main/java/com/android/dongnan/common/log/Logger.java
+++ common/src/main/java/com/android/dongnan/common/log/Logger.java
@@ -1,9 +1,22 @@
 package com.android.dongnan.common.log;
 
+import com.android.dongnan.common.BuildConfig;
+
 /**
  * Created by dream on 2017/2/9.
  */
 
 public class Logger {
-    private static Class<?> TAG = Logger.class;
+
+    private static final String TAG = "dream-->";
+    private static final boolean DEBUG_MODEL = BuildConfig.DEBUG_MODEL;
+
+    public static void v(Class<?> classes, String msg) {
+        android.util.Log.v(TAG + classes.getSimpleName(), msg);
+    }
+    
+    public static void v(Class<?> classes, String msg, Throwable throwable) {
+        android.util.Log.v(TAG + classes.getSimpleName(), msg, throwable);
+    }
+
 }
